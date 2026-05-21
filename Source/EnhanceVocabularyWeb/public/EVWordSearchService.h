8394a16b21f599509a32dfaa0da7cde2b96ac29b
IvanSliepov <slepow1985@gmail.com>
2026-05-21 03:36:35 -0500

    Add the files to handle the http calls

---
+++ Source/EnhanceVocabularyWeb/public/EVWordSearchService.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "EVWordSearchService.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENHANCEVOCABULARYWEB_API UEVWordSearchService : public UObject
+{
+	GENERATED_BODY()
+	
+};
